91869c98c4b3b6b1984add8ea1067d089a08fadb
hanfangluo <hl3554@columbia.edu>
2024-02-07 00:46:24 -0500

    a

--- src/components/courses/PHYS227/PHYS227.js
+++ src/components/courses/PHYS227/PHYS227.js
@@ -1,20 +1,24 @@
 import React from "react";
-import { Typography } from "@mui/material";
+import { Typography, Link } from "@mui/material";
 
 function PHYS227() {
+  // Assuming the PDF files are located in the 'public' directory
+  const ws2 = "courses/PHYS227/PHYS227_ws2.pdf";
+  const ws2_sol = "courses/PHYS227/PHYS227_ws2_sol.pdf";
+
   return (
     <div>
       <Typography variant="h6">PHYS227</Typography>
-      <iframe
-        src="/courses/PHYS227/PHYS227_ws2.pdf"
-        width="100%"
-        height="500px"
-      ></iframe>
-      <iframe
-        src="/courses/PHYS227/PHYS227_ws2_sol.pdf"
-        width="100%"
-        height="500px"
-      ></iframe>
+      <Typography>
+        <Link href={`/${ws2}`} target="_blank" rel="noopener noreferrer">
+          {"ws2"}
+        </Link>
+      </Typography>
+      <Typography>
+        <Link href={`/${ws2_sol}`} target="_blank" rel="noopener noreferrer">
+          {"ws2_sol"}
+        </Link>
+      </Typography>
     </div>
   );
 }
